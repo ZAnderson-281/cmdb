9eab9f5baff92e87ea9ca450365b3ceae3d6d83f
Zac <azac281@gmail.com>
2021-01-10 20:32:14 -0600

    updated layout and sidebars

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,25 @@
 import React from "react";
 
+import AppsIcon from "@material-ui/icons/Apps";
+
 import GeneralCard from "./GeneralCard";
 function Dashboard() {
   return (
     <div className="container">
-      <div className="dashboard-sidebar"></div>
+      <div className="dashboard-sidebar">
+        <div className="sidebar-link active-sidebar">
+          <i className="fas fa-th"></i>
+          <h6>Home</h6>
+        </div>
+        <div className="sidebar-link">
+          <i className="fas fa-th"></i>
+          <h6>Home</h6>
+        </div>
+        <div className="sidebar-link">
+          <i className="fas fa-cog"></i>
+          <h6>Home</h6>
+        </div>
+      </div>
       <section className="dashboard-body">
         <div className="dashboard-title">
           <h2>Dashboard</h2>
