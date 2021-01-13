f18f8989d4d70f56f4ab96c345213e163eab7324
Zac <azac281@gmail.com>
2021-01-12 19:06:28 -0600

    Added more functionality to adding widgets menu

--- src/components/Dashboard/WidgetCreatorModal.js
+++ src/components/Dashboard/WidgetCreatorModal.js
@@ -21,20 +21,28 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
-    // border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
+  },
+  formControl: {
+    margin: "1rem 0rem",
   },
 }));
 
 function WidgetCreatorModal({ handleAddWidget }) {
   const { isDashboardModalOpen, setIsDashboardModalOpen } = useGlobalContext();
   const classes = useStyles();
-  const [type, setType] = useState("gc");
+
+  const [type, setType] = useState("");
+  const [graphType, setGraphType] = useState("");
+
   const [name, setName] = useState("");
 
   const handleCardTypeChange = (e) => {
     setType(e.target.value);
+  };
+  const handleGraphTypeChange = (e) => {
+    setGraphType(e.target.value);
   };
   const handleCardNameChange = (e) => {
     setName(e.target.value);
@@ -54,9 +62,19 @@ function WidgetCreatorModal({ handleAddWidget }) {
         <Paper className={classes.paper}>
           <h3 className="title">Widget Menu</h3>
           <div className="modal-form">
+            {/* Name Select */}
+            <FormControl className={classes.formControl}>
+              <TextField
+                id="standard-name"
+                label="Card Name"
+                value={name}
+                autoFocus
+                onChange={handleCardNameChange}
+              />
+            </FormControl>
             {/* Type Select */}
             <FormControl className={classes.formControl}>
-              <InputLabel id="type-select-label">Type</InputLabel>
+              <InputLabel id="type-select-label">Card Type</InputLabel>
               <Select
                 labelId="type-select-label"
                 value={type}
@@ -68,15 +86,31 @@ function WidgetCreatorModal({ handleAddWidget }) {
                 <MenuItem value={"gr"}>Stats</MenuItem>
               </Select>
             </FormControl>
-            {/* Name */}
-            <FormControl className={classes.formControl}>
-              <TextField
-                id="standard-name"
-                label="Name"
-                value={name}
-                onChange={handleCardNameChange}
-              />
-            </FormControl>
+
+            {type === "gr" && (
+              <>
+                <h4>Extra Questions:</h4>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="type-select-label">Graph Type</InputLabel>
+                  <Select
+                    labelId="type-select-label"
+                    value={graphType}
+                    onChange={handleGraphTypeChange}
+                  >
+                    <MenuItem value={"graph"}>Graph</MenuItem>
+                    <MenuItem value={"bar"}>Bar</MenuItem>
+                    <MenuItem value={"pie"}>Pie</MenuItem>
+                    <MenuItem value={"line"}>Line</MenuItem>
+                  </Select>
+                </FormControl>
+              </>
+            )}
+            {type === "dc" && (
+              <>
+                <h4>Extra Questions:</h4>
+              </>
+            )}
+
             <Button
               onClick={() => {
                 handleAddWidget(type, name);
